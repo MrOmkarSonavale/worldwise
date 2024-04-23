f32a66125b40f18b82c89bbcb7f0e7dbd800e8b5
omkar sonavale <100135599+MrOmkarSonavale@users.noreply.github.com>
2024-04-23 09:53:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Homepage from "./pages/Homepage"
 import PageNotFound from "./pages/PageNotFound"
 import AppLayout from "./pages/AppLayout"
 import Login from "./pages/Login"
-import CityList from "./components/cityList"
+import CityList from "./components/CityList"
 import CountryList from "./components/CountryList"
 import City from "./components/City"
 import Form from "./components/Form";
